bf6f5a462cb4dd4cd570cd5ac6801262b71d86a7
Walter <jerry148@gmail.com>
2023-05-29 14:32:45 +0800

    change timecol format

--- sqlite-history.zsh
+++ sqlite-history.zsh
@@ -421,7 +421,7 @@ $seps') as argv, max(start_time) as max_start"
 
     local mst="datetime(max_start, 'unixepoch')"
     local dst="datetime('now', 'start of day')"
-    local timecol="strftime(case when $mst > $dst then '%H:%M' else '%d/%m' end, max_start, 'unixepoch', 'localtime') as time"
+    local timecol="strftime(case when $mst > $dst then '%Y-%m-%d %H:%M' else '%Y-%m-%d %H:%M' end, max_start, 'unixepoch', 'localtime') as time"
 
     selcols="${timecol}, ${selcols}, argv as cmd"
 
